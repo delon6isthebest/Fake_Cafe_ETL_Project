8430cf3bda9dfa5c05e8d9ebfe4c704b9bf0fee2
Sahl Khan <skg.skhan2@gmail.com>
2022-06-07 16:16:18 +0100

    Split transform 1NF to seperate tasks

--- src/transform_3nf.py
+++ src/transform_3nf.py
@@ -1,5 +1,9 @@
 # TODO: Transform data_frame to 1st Normal Form
+# First we will split the basket items into separate rows after each comma
 
+# Extract the size (first word)
+
+# Extract the price (after the last dash)
 
 # TODO: Transform data_frame to 2nd Normal Form
 
