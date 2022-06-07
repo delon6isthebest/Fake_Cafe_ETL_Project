79b21a3ac513dce3c0a7af6c323589365a95dcf4
Asha Nair <nairash53@gmail.com>
2022-06-07 13:53:29 +0100

    Code tested on test.csv

--- src/ticket_10.py
+++ src/ticket_10.py
@@ -5,9 +5,16 @@ def hash_func(customer_info):
     str_hash=customer_info.encode()                 # Encode the customer info using UTF-8 default
     return hashlib.sha256(str_hash).hexdigest()     # Hash using SHA-256
 
-def create_hash_feature(df,existing_feat):                    # Apply hashing function to the feature
-    if pd.dtypes(df[existing_feat])!=str:
+def create_hash_feature(df,existing_feat):
+    print(df.dtypes[existing_feat])                                       # Apply hashing function to the feature
+    if df.dtypes[existing_feat]!=str:
         df[existing_feat]=df[existing_feat].astype(str)
     df[existing_feat+'_hash'] = df[existing_feat].apply(lambda x:hash_func(x))
     df.drop(columns=existing_feat,inplace=True)                           # Drop the original feature
     return  df
+
+# # Test code with test.csv
+df=pd.read_csv('test.csv')                                          
+#print(df.head())
+
+print(create_hash_feature(df,'customer_name'))
