36da29305ea5381eeae780970507d301b008b45d
Asha Nair <nairash53@gmail.com>
2022-06-08 19:16:49 +0100

    Encryption code

--- src/ticket_10.py
+++ src/ticket_10.py
@@ -1,5 +1,8 @@
 import hashlib
+from cryptography.fernet import Fernet
+from faker import Faker
 import pandas as pd
+pd.set_option('display.max_colwidth', None)
 
 def hash_func(customer_info):
     str_hash=customer_info.encode()                 # Encode the customer info using UTF-8 default
@@ -16,5 +19,31 @@ def create_hash_feature(df,existing_feat):
 # # Test code with test.csv
 df=pd.read_csv('test.csv')                                          
 #print(df.head())
+#print(df['customer_name'].nunique())
 
-print(create_hash_feature(df,'customer_name'))
+#print(create_hash_feature(df,'customer_name'))
+
+def drop_column(df,column):
+    df.drop(columns=column,inplace=True)
+
+drop_column(df,'card_number')
+
+def suppress_pii(df,column):
+    fake=Faker()
+    df[column]=df[column].apply(lambda x:fake.name())
+    return df
+
+print(suppress_pii(df,'customer_name'))
+
+def encryption(value):
+    key = Fernet.generate_key()             # Instance the Fernet class with the key
+    fernet = Fernet(key)                    # then use the Fernet class instance 
+    encrypted_value = fernet.encrypt(value.encode())     #to encrypt the string string must be encoded to byte string before encryption
+    return encrypted_value
+
+def encrypt_pii(df,column):   
+        df[column]=df[column].apply(lambda x:encryption(x))
+        print(df[column])
+        return df
+
+print(encrypt_pii(df,'customer_name'))
