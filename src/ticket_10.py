bd228b71a02e5f56c36ed3d5caf4dd90500d5d57
tobeqartobz <tobeqarbernardo@gmail.com>
2022-06-09 13:32:55 +0100

    enc_decrpyt_updtes

--- src/ticket_10.py
+++ src/ticket_10.py
@@ -2,7 +2,7 @@ from cmath import nan
 import pandas as pd
 from cryptography.fernet import Fernet
 
-df = pd.read_csv('test.csv')
+df = pd.read_csv('test.csv')          #Reading csv file from src directory
 
 def generate_key():                        #Generates a key and save it into a file
     key = Fernet.generate_key()
@@ -13,14 +13,14 @@ def load_key():                                    #Loads the key named `secret.
     return open("secretkey.txt", "rb").read()
 
 def encryption(value):  
-    key = load_key()        
-    fernet = Fernet(key)                  
-    encrypted_value = fernet.encrypt(value.encode())   
-    return encrypted_value
+    key = load_key()        #Accessing previously generated key
+    fernet = Fernet(key)        #Instance the fernet class with the key          
+    encrypted_value = fernet.encrypt(value.encode())   #use the instance to encrypt str, str as to be encoded to bytes str
+    return encrypted_value                            #Return encrypted str
 
 def encrypt_pii(df,column):   
-    df[column]=df[column].apply(lambda x:encryption(x))
-    return df
+    df[column]=df[column].apply(lambda x:encryption(x)) #enrypting the column
+    return df            #return the df with encrypted column
 
 print(encrypt_pii(df,'customer_name'))                   #prints dataframe with the pii column decrypted
 
@@ -34,7 +34,7 @@ def decrypt_message(encrypted_value):             #To decrypt an encrypted messa
     return decrypted_msg
 
 def decrypt_pii(df,column):
-    df[column]=df[column].apply(lambda x:decrypt_message(x))
-    print(df[column])
-    return df
+    df[column]=df[column].apply(lambda x:decrypt_message(x)) #decrypts the previously encrypted column
+    print(df[column])  #prints decrypted column
+    return df                  #return the df with decrypted column
 print(decrypt_pii(df,'customer_name'))               #prints dataframe with the pii column decrypted
