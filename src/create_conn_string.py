15af9af26c32047e716ec1c942f71d03e1a70369
bfairnington <bfairnington@hotmail.com>
2022-06-16 17:45:09 +0100

    commit remove host

--- src/create_conn_string.py
+++ src/create_conn_string.py
@@ -3,13 +3,12 @@ import os
 
 def create_conn_string():
     load_dotenv()
-    postgresql_host = os.environ.get("postgresql_host")
     postgresql_user = os.environ.get("postgresql_user")
     postgresql_pass = os.environ.get("postgresql_pass")
     postgresql_db = os.environ.get("postgresql_db")
 
     # Creating connection string
-    conn_string = f'postgresql://{postgresql_user}:{postgresql_pass}@{postgresql_host}/{postgresql_db}'
+    conn_string = f'postgresql://{postgresql_user}:{postgresql_pass}@/{postgresql_db}'
 
     return conn_string
 
