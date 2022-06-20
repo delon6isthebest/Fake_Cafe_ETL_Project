fb97bea4c951798f276725c40a4a15ad8f1bae01
bfairnington <bfairnington@hotmail.com>
2022-06-20 15:07:47 +0100

    product query update

--- src/product_query.py
+++ src/product_query.py
@@ -1,9 +1,7 @@
 # Situation product details from the dataframe has been loaded into the table
 
 # Import libraries
-from extractcsv import *
 from create_db import *
-# from transform_3nf import *
 import pandas as pd
 
 # Query database with unique combinations of name, size, flavour and price
@@ -14,8 +12,8 @@ def query_id(conn, cursor, row_index: int, products_df:pd.DataFrame):
     products_name = products_df['name'].loc[row_index]
     products_size = products_df['size'].loc[row_index]
     products_flavour = products_df['flavour'].loc[row_index]
-
-    cursor.execute(f'SELECT id FROM productsmvp WHERE name = {products_name} AND size = {products_size} AND flavour = {products_flavour}')
-    sql_id = cursor.fetchone()
+    values = [products_name, products_size, products_flavour]
+    cursor.execute(f'SELECT id FROM productsmvp WHERE name = %s AND size = %s AND flavour = %s', values)
+    sql_id = cursor.fetchone()[0]
 
     return sql_id
