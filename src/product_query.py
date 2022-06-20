5126e02c43ebd3e1f05f78203f1683dc611634c0
Sahl Khan <skg.skhan2@gmail.com>
2022-06-20 12:36:29 +0100

    Apply query command to each product, and replace product_id column with queried values

--- src/product_query.py
+++ src/product_query.py
@@ -8,16 +8,32 @@ import pandas as pd
 # Query database with unique combinations of name, size, flavour and price
 # Connection and cursor objects already exist
 # Access the values in DF given the row index
-def query_id(conn, cursor, row_index: int, df:pd.DataFrame, table_name: str):
-    [name, size, flavout, price] = df
+def query_id(conn, cursor, row_index: int, df:pd.DataFrame):
+    values = df[['name', 'size', 'flavour']].iloc[row_index]
 
 
     cursor.execute(f'SELECT * FROM products')
     rows = cursor.fetchall()
+    select_query =\
     """
-    SELECT name, size, flavour, price
-    );
+    SELECT name, size, flavour
+    FROM products
+    WHERE name = %s, size = %s, flavour = %s 
+    ;
     """
+    # Note values is a list of values to be inserted into the SQL command at the placeholders
+    cursor.execute(select_query, values) 
+    row = cursor.fetchone()
+    return row[0]
+
+# Store queried products id's in a separate column
+def query_product_ids(conn, cursor, df:pd.DataFrame):
+    df['queried_id'] = (df.index).apply(lambda x : query_id(conn, cursor, x, df))
+
+# After querying ids, replace them in the basket_table
+def replace_index_with_queried_id(df, products_df):
+    df['product_id'] = df['product_id'].apply(lambda x: products_df['queried_id'].iloc[x])
+    return df
 
 
 
