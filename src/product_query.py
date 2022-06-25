eea56076481312880e3440bd59d9c158498ca0bf
Asha Nair <nairash53@gmail.com>
2022-06-25 18:06:25 +0100

    no changes to product query

--- src/product_query.py
+++ src/product_query.py
@@ -1,6 +1,7 @@
 # Situation product details from the dataframe has been loaded into the table
 
 # Import libraries
+from distutils.sysconfig import customize_compiler
 from transform_3nf import * # Only needed when testing out how they combine
 from create_db import *
 import pandas as pd
@@ -29,19 +30,26 @@ def replace_index_with_queried_id(df, products_df):
     df['product_id'] = df['product_id'].apply(lambda x: products_df['queried_id'].loc[x])
     return df
 
+
+                                                    
+                                                    
 if __name__ == "__main__":
     # Connect to db and store in variable by creating the connection object and the cursor object
     (conn, cursor) = connect_to_db()
-    df = pd.read_csv(TEST_CSV)
-    del df['card_number']
-    df = split_basket_items(df)
-    extract_product_details(df)
-    products_df = create_products_df(df)
-    transactions_df = create_transactions_df(df, products_df)
-    query_product_ids(conn, cursor, products_df)
-    print(products_df)
-    print()
-    print(transactions_df)
-    print()
-    replace_index_with_queried_id(transactions_df, products_df)
-    print(transactions_df)
+    # df = pd.read_csv(TEST_CSV)
+    # del df['card_number']
+    # df = split_basket_items(df)
+    # extract_product_details(df)
+    # products_df = create_products_df(df)
+    # transactions_df = create_transactions_df(df, products_df)
+    # query_product_ids(conn, cursor, products_df)
+    # print(products_df)
+    # print()
+    # print(transactions_df)
+    # print()
+    # replace_index_with_queried_id(transactions_df, products_df)
+    # print(transactions_df)
+
+
+    
+    
