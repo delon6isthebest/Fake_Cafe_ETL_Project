61d561b20308e20d4e00af0012d7af7f50de6af7
Sahl Khan <skg.skhan2@gmail.com>
2022-07-03 21:13:59 +0100

    Reverted third normal form logic whilst incorporating UUID

--- src/transform_3nf.py
+++ src/transform_3nf.py
@@ -6,24 +6,26 @@ import uuid
 #from product_query import query_id
 TEST_CSV = "test.csv" # Located in 'src' folder
 BASKET_COLUMN = "basket_items"
+# Columns to be extracted for products_df, index column acts as a local primary key
 PRODUCT_COLUMNS = ["name", "size", "flavour"]
-#TRANSACTION_COLUMNS = ["timestamp", "store", "customer_name", "total_price", "cash_or_card"]
-TRANSACTION_COLUMNS = ["transaction_id","timestamp", "store", "customer_name", "total_price", "cash_or_card"]
-#BASKET_ITEMS_COLUMNS = ["transaction_id", "product_id", "price", "quantity"]
-BASKET_ITEMS_COLUMNS = ["transaction_id", "product_id", "price", "quantity","timestamp", "store", "customer_name", "total_price", "cash_or_card","basket_items"]
+TRANSACTION_UUID = "id"
+# Columns to be extracted for transactions_df, after UUIDs have been generated
+TRANSACTION_COLUMNS = [TRANSACTION_UUID, "timestamp", "store", "customer_name", "total_price", "cash_or_card"]
+# TRANSACTION_COLUMNS = ["transaction_id","timestamp", "store", "customer_name", "total_price", "cash_or_card"]
 TRANSACTION_ID = "transaction_id"
 PRODUCT_ID = "product_id"
+BASKET_ITEMS_COLUMNS = [TRANSACTION_ID, PRODUCT_ID, "price", "quantity"]
+# BASKET_ITEMS_COLUMNS = ["transaction_id", "product_id", "price", "quantity","timestamp", "store", "customer_name", "total_price", "cash_or_card","basket_items"]
 TRANSACTIONS_TABLE = "transactions"
 PRODUCTS_TABLE = "products"
 BASKET_ITEMS_TABLE = "basket_items"
 
-# TODO: Transform data_frame to 1st Normal Form
-# First we will split the basket items into separate rows after each comma (counting quantity per basket)
+# Transform data_frame to 1st Normal Form
+# We will split the basket items into separate rows after each comma (counting quantity per basket)
 def split_basket_items(df:pd.DataFrame) -> pd.DataFrame:
-    
-    df[TRANSACTION_ID] = df.apply(lambda _: uuid.uuid4(), axis=1)
+    # df[TRANSACTION_ID] = df.apply(lambda _: uuid.uuid4(), axis=1)
     #df[TRANSACTION_ID] = df.index
-    def count_items(basket: str) -> list:
+    def count_items(basket: str):
         item_count_dict = {}
         items_list = [item.strip() for item in basket.split(",")]
         for item in items_list:
@@ -32,7 +34,7 @@ def split_basket_items(df:pd.DataFrame) -> pd.DataFrame:
     df[BASKET_COLUMN] = df[BASKET_COLUMN].apply(lambda x: count_items(x))
     return df.explode(BASKET_COLUMN, ignore_index=True)
 
-# Split the product column into separate columns for each detail: size, name, flavour, price
+# Split the product column into separate columns for each detail: size, name, flavour, price, quantity
 def extract_product_details(df:pd.DataFrame) -> pd.DataFrame:
     # Extract the details of product: size, name, flavour, price
     def extract_details(product_count:tuple) -> list:
@@ -53,41 +55,49 @@ def extract_product_details(df:pd.DataFrame) -> pd.DataFrame:
         return details
     df[BASKET_COLUMN] = df[BASKET_COLUMN].apply(extract_details)
     df[['size','name','flavour','price','quantity']] = pd.DataFrame(df[BASKET_COLUMN].to_list())
-    #del df[BASKET_COLUMN]
+    del df[BASKET_COLUMN]
     return df
 
 # Extract some columns for a subtable: 'transactions' and 'products', and replace with row_index
-def extract_subtable(df:pd.DataFrame, subcolumns, foreign_key:str):
+def extract_subtable(df:pd.DataFrame, subcolumns:list[str], foreign_key:str):
     subtable_df = df[subcolumns].drop_duplicates()
-    subtable_df['combined'] = subtable_df.astype(str).apply(np.sum, axis=1)
-    
-    def get_index(combined_value, subtable_df):
-        try:
-            return subtable_df.index[subtable_df['combined'] == combined_value].tolist()[0]
-        except:
-            return None
 
-    df[foreign_key] = df[subcolumns].astype(str).apply(np.sum, axis=1).apply(lambda x: get_index(x, subtable_df))
+    if foreign_key != TRANSACTION_ID:
+        subtable_df['combined'] = subtable_df.astype(str).apply(np.sum, axis=1)
+        
+        def get_index(combined_value:str, subtable_df:pd.DataFrame):
+            try:
+                return subtable_df.index[subtable_df['combined'] == combined_value].tolist()[0]
+            except:
+                return None
+
+        df[foreign_key] = df[subcolumns].astype(str).apply(np.sum, axis=1).apply(lambda x: get_index(x, subtable_df))
+        del subtable_df['combined']
+    else:
+        df[TRANSACTION_ID] = df[TRANSACTION_UUID] # UUIDs need to be generated before extraction
+
     for column in subcolumns:
-        del df[column]
-    del subtable_df['combined']
+        if column != TRANSACTION_ID:
+            del df[column]
+    
     return subtable_df
 
 
 # Normalise the transactions: Replace product details with product_id
 
-# TODO: Transform data_frame to 3rd Normal Form
+# Transform data_frame to 3rd Normal Form
 
-def transform_3nf(data_df: pd.DataFrame) -> dict[str, pd.DataFrame]:
-    #print(data_df.head())
-    
-    # customers_df = extract_subtable(transactions_df, ['customer_name'], 'customer_id')
-
-    basket_df = split_basket_items(data_df)
+def third_normal_form(data_df: pd.DataFrame) -> dict[str, pd.DataFrame]:
+    #TODO: Add UUIDs in column data_df[TRANSACTION_UUID]
+    data_df[TRANSACTION_UUID] = data_df.index
+    # Start 3NF
     transactions_df = extract_subtable(data_df, TRANSACTION_COLUMNS, TRANSACTION_ID)
+    # customers_df = extract_subtable(transactions_df, ['customer_name'], 'customer_id')
+    # stores_df = extract_subtable(transactions_df, ['store'], 'store_id')
+    basket_df = split_basket_items(data_df)
     extract_product_details(basket_df)
     products_df = extract_subtable(basket_df, PRODUCT_COLUMNS, PRODUCT_ID)
-    basket_df=basket_df
+    
     return {
         PRODUCTS_TABLE: products_df,
         TRANSACTIONS_TABLE: transactions_df,
@@ -117,7 +127,7 @@ if __name__ == "__main__":
     data_df = pd.read_csv(TEST_CSV)
     del data_df['card_number']
     #print(data_df)
-    rt=transform_3nf(data_df)
+    rt=third_normal_form(data_df)
     
     rt['basket_items']=replace_index_with_queried_id(rt['basket_items'])
     rt['basket_items']=rt['basket_items'].drop(['timestamp',
