f462f48181ad089aba25045b59bef10629f64284
tobeqartobz <tobeqarbernardo@gmail.com>
2022-07-11 18:19:49 +0100

    final_stk_updte

--- src/deployment_packages.sh
+++
@@ -1,6 +0,0 @@
-cd .venv/lib/python3.9/site-packages
-docker run -v "$PWD":/var/task "lambci/lambda:build-python3.8" /bin/sh -c "pip install -r requirements.txt -t package/; exit"
-cd package
-zip -r ../deployment_package.zip .   
-cd ..
-zip -g deployment_package.zip lambda_function.py
---
+++ src/validate_template.sh
@@ -0,0 +1,1 @@
+aws cloudformation validate-template --template-body 'file://C:/Users/dawid/team-1-project/templates/T1cloudformation.yaml'
